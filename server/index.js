dfecdcb6882e18cb335b62bfaf36a287aaab8a07
ehellgre <emil.hellgren@edu.lapinamk.fi>
2023-01-28 23:45:08 +0200

    bug fixes & mock data added

--- server/index.js
+++ server/index.js
@@ -14,6 +14,11 @@ import postRoutes from "./routes/posts.js"
 import { register } from "./controllers/auth.js"
 import { createPost } from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js"
+import User from "./models/User.js"
+import Post from "./models/Post.js"
+import { users, posts } from "./data/index.js"
+
+
 
 /* CONFIGS */
 const __filename = fileURLToPath(import.meta.url) // For grabbing file url (when using type: modules) 
@@ -67,5 +72,9 @@ mongoose
     })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`)) // After we connect do callback function
+
+        /* ADD MOCK DATA ONE TIME */
+        //User.insertMany(users)
+        //Post.insertMany(posts)
     })
     .catch((error) => console.log(`${error} did not connect`)) // Catch if we have an error connecting
