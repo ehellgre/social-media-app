fe0b3865f219cd1177687d99eafbf306079687a4
ehellgre <emil.hellgren@edu.lapinamk.fi>
2023-01-31 19:56:36 +0200

    theme & darkmode

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/profilePage";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
+import HomePage from "scenes/homePage"
+import LoginPage from "scenes/loginPage"
+import ProfilePage from "scenes/profilePage"
+import { useMemo } from "react"
+import { useSelector } from "react-redux"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from "./theme"
+
+
 
 function App() {
+  const mode = useSelector((state) => state.mode) /* Grab the mode value in the state*/
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]) /* Set up the theme */
 
   return <div className="app">
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} /> {/* :userId = param that we can set, if you go profile/[userid] we hit this route */}
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} /> {/* :userId = param that we can set, if you go profile/[userid] we hit this route */}
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   </div>
 }
--- client/src/index.js
+++ client/src/index.js
@@ -30,7 +30,7 @@ const store = configureStore({
     }),
 })
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); /**/
 root.render(
   <React.StrictMode>
     <Provider store={store}>
