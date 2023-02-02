677023fa27347dcf23a8b6b331e78c49f1279f18
ehellgre <emil.hellgren@edu.lapinamk.fi>
2023-02-02 17:41:12 +0200

    some styling

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ function App() {
   return <div className="app">
     <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system"
+
+/* Styled component */
+/* For reusing css as a component */
+const FlexBetween = styled(Box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+})
+
+export default FlexBetween
